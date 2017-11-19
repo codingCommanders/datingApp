759c563e86421d5d8f513f0dbe00699a9175fd86
candy66 <codingcommanders@gmail.com>
2017-11-19 16:23:46 -0500

    changed some logic calculations

--- index.php
+++ index.php
@@ -18,14 +18,14 @@
      "youremail@email.com" required><br>
      <!-- q1-->
      <label>How well can you program?</label><br>
-     <select for="q1">
+     <select name="q1">
        <option for="q1" value="1">Expert</option>
        <option for="q1" value="2">Average</option>
        <option for="q1" value="3">Below Average/No Experience</option>
      </select><br> 
      <!-- q2-->
      <label>What is your ideal first date?</label><br>
-     <select for="q2">
+     <select name="q2">
        <option for="q2" value="1">Coffee Shop</option>
        <option for="q2" value="2">Dinner and Computer Programming</option>
        <option for="q2" value="3">Your House</option>
@@ -34,14 +34,14 @@
      </select><br>
      <!-- q3-->
      <label>What is your highest level of education?</label><br>
-     <select for="q3">
+     <select name="q3">
        <option for="q3" value="1">No College Degree</option>
        <option for="q3" value="2">BA/BS</option>
        <option for="q3" value="3">Graduate Degree</option>
      </select><br>
      <!-- q4-->
      <label>What is your idea of a romantic evening?</label><br>
-     <select for="q4">
+     <select name="q4">
        <option for="q4" value="1">Romantic Comedy</option>
        <option for="q4" value="2">PluralSight and Chill</option>
        <option for="q4" value="3">"Hey baby, do you wanna come over?</option>
@@ -53,10 +53,10 @@
      <!-- q6-->
      <label>Do you know embedded systems?</label><br>
      <input type="radio" name="q6" value="1">Yes</input><br>
-     <input type="radio" name="q6" value="1">No</label><br>
+     <input type="radio" name="q6" value="2">No</label><br>
      <!-- q7-->
      <label>We are coding at your house.  You order in dinner.  When the dinner arrives, my order is wrong.  What do you do?</label><br>
-     <select for="q7">
+     <select name="q7">
        <option for="q7" value="1">Call and have my order corrected</option>
        <option for="q7" value="2">Call and get a get a free meal the next time you order.</option>
        <option for="q7" value="3">Ask me how to proceed</option>
